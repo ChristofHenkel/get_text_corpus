4ab8ee619f034dd0bd9245caa9ee0e597f5c546a
Christof Henkel <henkel.christof@gmail.com>
2017-08-21 12:17:05 +0200

    some improvements

--- download_news_en.sh
+++
@@ -1,3 +0,0 @@
-wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.en.shuffled.gz
-wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2010.en.shuffled.gz
-wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.en.shuffled.gz
--- download_wiki.sh
+++
@@ -1,1 +0,0 @@
-wget http://download.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
